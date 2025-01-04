45348ae08bda404c6fbc50bf1f0612f9570bf9ab
Tony Nguyen <tonynguyeenn@gmail.com>
2025-01-03 21:44:19 -0800

    Setting up auth

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +15,28 @@ const firebaseConfig = {
   storageBucket: "prep-df000.firebasestorage.app",
   messagingSenderId: "605576166315",
   appId: "1:605576166315:web:d9fb8a23d3cf7b1a6070ee",
-  measurementId: "G-D053J54DGN"
+  measurementId: "G-D053J54DGN",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+let analytics;
+/** @type {import('firebase/firestore').Firestore} */
+let db;
+/** @type {import('firebase/auth').Auth} */
+let auth;
+let app;
+if (firebaseConfig?.projectId) {
+  // Initialize Firebase
+  app = initializeApp(firebaseConfig);
+
+  if (app.name && typeof window !== "undefined") {
+    analytics = getAnalytics(app);
+  }
+
+  // Access Firebase services using shorthand notation
+  db = getFirestore(app);
+  auth = getAuth(app);
+}
+
+export { app, analytics, db, auth };
---
+++ src/components/navbar/loginForm.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "@/app/firebase";
+
+function LoginForm() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log("User logged in:", userCredential.user);
+    } catch (error) {
+      console.error("Error logging in:", error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin} className="flex flex-col gap-6">
+      <div>
+        <h2 className="text-2xl font-bold">Email</h2>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          className="border-b-[3px] border-b-black bg-transparent text-xl w-full"
+        />
+      </div>
+      <div>
+        <h2 className="text-2xl font-bold">Password</h2>
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          className="border-b-[3px] border-b-black bg-transparent text-xl w-full"
+        />
+      </div>
+      <button
+        type="submit"
+        className="bg-[#E5D3FF] border-[#CCA8FF] border-[4px] rounded-lg py-1 font-bold text-xl"
+      >
+        Log in
+      </button>
+    </form>
+  );
+}
+
+export default LoginForm;
