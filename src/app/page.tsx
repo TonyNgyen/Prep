3e7c2c91bc2c99426b1f2694225e2915fd1412e6
Tony Nguyen <tonynguyeenn@gmail.com>
2025-01-08 23:27:50 -0800

    Working on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import NavBar from "@/components/navbar/navbar";
+import { MdAccountCircle } from "react-icons/md";
 import { useState } from "react";
 
 export default function Home() {
@@ -8,10 +9,26 @@ export default function Home() {
     return <div>Not Logged In</div>;
   } else {
     return (
-      <div>
-        <div>Hi User!</div>
-        <div>Log Meal</div>
-        <div>Nutritional Value</div>
+      <div className="px-4">
+        <div className="pt-10 flex justify-between items-center">
+          <h1 className="text-4xl font-bold">Hello User</h1>
+          <a href="/account" className="flex items-center">
+            <button className="">
+              <MdAccountCircle className="h-[45px] w-[45px]" />
+            </button>
+          </a>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="bg-mainGreen p-4 text-white rounded-lg shadow-md">
+            Log Meal
+          </div>
+          <div className="bg-white p-4 text-black rounded-lg shadow-md">
+            Nutritional Values
+          </div>
+          <div className="bg-white p-4 text-black rounded-lg shadow-md">
+            Weight History
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,10 +2,10 @@
 
 import React, { useState } from "react";
 import { IoHome } from "react-icons/io5";
-import { LuHistory } from "react-icons/lu";
+import { AiOutlineHistory } from "react-icons/ai";
 import { IoIosAddCircle } from "react-icons/io";
-import { MdAccountCircle } from "react-icons/md";
-import { FaGear } from "react-icons/fa6";
+import { MdInventory } from "react-icons/md";
+import { MdEdit } from "react-icons/md";
 
 function NavBar() {
   const [user, setUser] = useState(true);
@@ -27,30 +27,30 @@ function NavBar() {
     return (
       <div className="w-full py-5 lg:px-20 px-5 flex justify-between items-center fixed bottom-0 left-0 bg-mainGreen h-20">
         <a href="/">
-          <button className="w-[60px] h-[60px] flex flex-col items-center justify-center text-white gap-1">
+          <button className="w-[55px] h-[55px] flex flex-col items-center justify-center text-white gap-1">
             <IoHome className="w-full h-full" />
             <p className="font-semibold text-sm">Home</p>
           </button>
         </a>
         <a href="/history">
-          <button className="w-[60px] h-[60px] flex flex-col items-center justify-center text-white gap-1">
-            <LuHistory className="w-full h-full" />
+          <button className="w-[55px] h-[55px] flex flex-col items-center justify-center text-white gap-1 ">
+            <AiOutlineHistory className="w-full h-full" />
             <p className="font-semibold text-sm">History</p>
           </button>
         </a>
-        <button className="w-[75px] h-[75px] flex flex-col items-center justify-center text-white rounded-full">
+        <button className="w-[70px] h-[70px] flex flex-col items-center justify-center text-white rounded-full">
           <IoIosAddCircle className="w-full h-full" />
         </button>
-        <a href="account">
-          <button className="w-[60px] h-[60px] flex flex-col items-center justify-center text-white gap-1">
-            <MdAccountCircle className="w-full h-full" />
-            <p className="font-semibold text-sm">Account</p>
+        <a href="/edit">
+          <button className="w-[55px] h-[55px] flex flex-col items-center justify-center text-white gap-1 ">
+            <MdEdit className="w-full h-full" />
+            <p className="font-semibold text-sm">Edit</p>
           </button>
         </a>
-        <a href="settings">
-          <button className="w-[60px] h-[60px] flex flex-col items-center justify-center text-white gap-1">
-            <FaGear className="w-full h-full" />
-            <p className="font-semibold text-sm">Settings</p>
+        <a href="/stock">
+          <button className="w-[55px] h-[55px] flex flex-col items-center justify-center text-white gap-1">
+            <MdInventory className="w-full h-full" />
+            <p className="font-semibold text-sm">Stock</p>
           </button>
         </a>
       </div>
