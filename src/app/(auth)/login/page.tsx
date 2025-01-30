025c0834f60789a0b68e87c71b49f09ed0e7273e
Tony Nguyen <tonynguyeenn@gmail.com>
2025-01-30 00:35:12 -0800

    Preparing move to supabase

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,13 +1,14 @@
-import LoginForm from "@/components/auth/loginForm/loginForm";
-import NavBar from "@/components/navbar/navbar";
-import React from "react";
+import { login, signup } from "./actions";
 
-function LoginPage() {
+export default function LoginPage() {
   return (
-    <div className="">
-      <LoginForm />
-    </div>
+    <form>
+      <label htmlFor="email">Email:</label>
+      <input id="email" name="email" type="email" required />
+      <label htmlFor="password">Password:</label>
+      <input id="password" name="password" type="password" required />
+      <button formAction={login}>Log in</button>
+      <button formAction={signup}>Sign up</button>
+    </form>
   );
 }
-
-export default LoginPage;
