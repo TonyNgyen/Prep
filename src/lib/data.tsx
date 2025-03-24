386e04d145505a2b2fec15489d1e17abce1c58e0
Tony <93945730+TonyNgyen@users.noreply.github.com>
2025-03-23 23:50:51 -0700

    Cleaning up code

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -2,50 +2,6 @@ import { InventoryIngredient, InventoryRecipe, NutritionFacts } from "@/types";
 import { createClient } from "@/utils/supabase/client";
 import { addNutrition } from "./functions";
 
-const NUTRITIONAL_KEYS = {
-  calories: "Calories",
-  protein: "Protein",
-  totalFat: "Total Fat",
-  saturatedFat: "Saturated Fat",
-  polyunsaturatedFat: "Polyunsaturated Fat",
-  monounsaturatedFat: "Monounsaturated Fat",
-  transFat: "Trans Fat",
-  cholesterol: "Cholesterol",
-  sodium: "Sodium",
-  potassium: "Potassium",
-  totalCarbohydrates: "Total Carbohydrates",
-  sugars: "Sugars",
-  addedSugars: "Added Sugars",
-  sugarAlcohols: "Sugar Alcohols",
-  vitaminA: "Vitamin A",
-  vitaminC: "Vitamin C",
-  vitaminD: "Vitamin D",
-  calcium: "Calcium",
-  iron: "Iron",
-};
-
-const NUTRITIONAL_UNITS: Record<string, string> = {
-  calories: "kcal",
-  protein: "g",
-  totalFat: "g",
-  saturatedFat: "g",
-  polyunsaturatedFat: "g",
-  monounsaturatedFat: "g",
-  transFat: "g",
-  cholesterol: "mg",
-  sodium: "mg",
-  potassium: "mg",
-  totalCarbohydrates: "g",
-  sugars: "g",
-  addedSugars: "g",
-  sugarAlcohols: "g",
-  vitaminA: "%",
-  vitaminC: "%",
-  vitaminD: "%",
-  calcium: "%",
-  iron: "%",
-};
-
 type ItemsToAdd = Record<string, InventoryIngredient | InventoryRecipe>;
 
 type DailyMealEntry = {
